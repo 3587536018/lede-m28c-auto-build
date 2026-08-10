d88c76753e479c030117024dfc7c4ceda41ed933
3587536018 <3587536018@users.noreply.github.com>
2026-08-10 20:28:47 +0000

    feat: 4G rootfs, argon theme, dockerman/ddns-go/adguardhome/istore, auto-expand data partition

--- prepare.sh
+++ prepare.sh
@@ -17,13 +17,7 @@ fi
 #cat ../m28c.config > .config
 cat feeds.conf.default > feeds.conf
 echo "" >> feeds.conf
-echo "src-git qmodem https://github.com/FUjr/QModem.git;main" >> feeds.conf
+echo "src-git qmodem https://github.com/FUjr/QModem.git;main
+src-git istore https://github.com/linkease/istore.git;main" >> feeds.conf
 rm -rf files
 cp -r ../files .
-if [ -d "package/zz/luci-theme-alpha" ]; then
-    cd package/zz/luci-theme-alpha
-    git pull || { echo "luci-theme-alpha git pull failed"; exit 1; }
-    cd ../../..
-else
-    git clone https://github.com/derisamedia/luci-theme-alpha.git package/zz/luci-theme-alpha || { echo "luci-theme-alpha git clone failed"; exit 1; }
-fi
